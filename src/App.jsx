332f426bffee09e82867a25374fd1530dca32a42
Khemraj Shrestha <khemrajshrestha471@gmail.com>
2023-05-20 12:10:22 +0545

    yy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 import App_helper from './App_helper'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
@@ -7,17 +8,25 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Certificates from './components/certificates/Certificates'
 
 const App = () => {
-  return (
-    <>
-    <Router>
-      <Routes>
-      <Route path='/' element={<Certificates />} />
-      <Route path='/certificates' element={<App_helper />} />
-      </Routes>
-    </Router>
-    </>
 
-  )
+    return (
+      <>
+      <Router>
+        <Routes>
+        <Route path='/' element={<App_helper />} />
+        <Route exact path='/certificates' element={<Certificates />} />
+        </Routes>
+      </Router>
+      </>
+    )
+
+
+
+
+
+
+  
+
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App/>, document.querySelector("#root"))
+
+
+ReactDOM.render(
+    <App/>
+        
+
+
+,
+     document.querySelector("#root")
+ );
