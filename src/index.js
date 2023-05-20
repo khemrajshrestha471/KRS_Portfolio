89ddaa3fd57b99c24163ea8c2266a9bbfd61876e
Khemraj Shrestha <khemrajshrestha471@gmail.com>
2023-05-20 12:24:35 +0545

    erfds

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import { HashRouter } from 'react-router-dom';
 import App from './App'
 import './index.css'
-
-import { HashRouter } from 'react-router-dom';
-
 
 
 ReactDOM.render(
     <React.StrictMode>
-        {/* <HashRouter> */}
-            <App/>
-            {/* </HashRouter> */}
-
-    </React.StrictMode>
-    
-        
-
-
-,
-     document.querySelector("#root")
- );
+        <HashRouter>
+            <App />
+        </HashRouter>
+    </React.StrictMode>,
+    document.querySelector("#root")
+);
