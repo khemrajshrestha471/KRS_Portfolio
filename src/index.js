f6acb135e73463a980ee778dd4db2c0d6db514d7
Khemraj Shrestha <khemrajshrestha471@gmail.com>
2023-05-20 12:13:58 +0545

    hh

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
+import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 
+import { HashRouter } from 'react-router-dom';
+
 
 
 ReactDOM.render(
-    <App/>
+    <React.StrictMode>
+        {/* <HashRouter> */}
+            <App/>
+            {/* </HashRouter> */}
+
+    </React.StrictMode>
+    
         
 
 
